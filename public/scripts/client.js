e29dea2b8d84e5bf8598b3bc1c1902524e030866
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-26 13:29:51 -0400

    update to default profile pic.  Adds JS functionality to update the character count.  Made counter an Id instead of a class.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,15 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const input = document.querySelector('textarea');
+const log = document.getElementById('counter');
+
+input.addEventListener('input', updateValue);
+function updateValue(e) {
+  log.textContent= e.target.value
+  count = log.textContent.length;
+  char = 140;
+  char = 140 - count;
+  console.log(char)
+  document.getElementById('counter').innerHTML = char;
+}
