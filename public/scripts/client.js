65195b2de4c3918362eee80d85c5392ed1b57b80
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-26 14:21:53 -0400

    Added some JS to auto-resize the text area field so that when a user reaches a new line it keeps all text in frame

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,4 +4,13 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  $('#tweet-text').on('input', function () {
+    this.style.height = 'auto';
+      
+    this.style.height = 
+            (this.scrollHeight) + 'px';
+});
 
+
+});
