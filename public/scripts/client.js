d2dc2c68b8f86fb425ad2c295a2356fb15600383
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-27 13:02:38 -0400

    Adds a function to convert a tweet object into HTML.  Corrects some minor bugs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,13 +4,63 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//converts tweet object into HTML formatted tweet to be displayed on page.
+  const createTweetElement = function(tweetData) {
+    let $tweet = `
+    <article class="tweet">
+    <header>
+      <span class="user">
+        <img src="${tweetData.user.avatars}">
+        <p class="user">${tweetData.user.name}</p>
+      </span>
+      <span class="handle">${tweetData.user.handle}</span>
+    </header>
+    <p class="tweet-content">
+      ${tweetData.content.text}
+    </p>
+    <footer>
+      <time>${tweetData.created_at}</time>
+      <div class="icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>           
+    </footer>
+  </article>
+  `;
+  // console.log("function tweet:",$tweet);
+  return $tweet;
+
+  };
+
+const tweetEx =
+{
+  "user": {
+          "name": "Descartes",
+          "avatars": "https://i.imgur.com/nlhLi3I.png",
+          "handle": "@rd"
+  },
+"content": {
+            "text": "Je pense , donc je suis"
+  },
+"created_at": 1650818297550
+};
+
+
+
 $(document).ready(function() {
+
+  // auto-expands the textarea for multi-line tweets.
   $('#tweet-text').on('input', function () {
     this.style.height = 'auto';
       
     this.style.height = 
             (this.scrollHeight) + 'px';
-});
+  });
 
+  const $tweet = createTweetElement(tweetEx);
+
+  console.log("tweet:",$tweet);
+  $('#tweet-container').append($tweet); 
 
 });
